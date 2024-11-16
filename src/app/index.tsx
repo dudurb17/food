a71775d0591a55cd888882892f9aa870ad1747d0
dudurb17 <eduardo.robettibedin@gmail.com>
2024-11-16 20:02:33 -0300

    started nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,14 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Tesn.</Text>
+    <View className="w-full h-full flex justify-center items-center">
+      <Text className="text-red-600">Tesn.</Text>
     </View>
   );
 }
